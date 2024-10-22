c9008e91c013793478c94e687baa6b157a9ba2f6
Will Young <numino@hotmail.co.uk>
2024-10-22 18:57:38 +0100

    Initial commit

---
+++ obj/Debug/net6.0/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace SuperStash
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "SuperStash";
+        public const string PLUGIN_NAME = "SuperStash";
+        public const string PLUGIN_VERSION = "0.1.10";
+    }
+}
